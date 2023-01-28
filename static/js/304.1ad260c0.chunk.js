f5f66fc6be9895d995711ec11fde49df6a622fb2
YuliiaMakhovska <92719550+YuliiaMakhovska@users.noreply.github.com>
2023-01-28 18:26:02 +0000

    Deploying to gh-pages from @ YuliiaMakhovska/goit-react-hw-08-phonebook@508c5b9a395bf3e6dfb4f6680fd91c89f86f06da 🚀

--- static/js/304.1ad260c0.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[304],{304:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r,c,o=t(168),a=t(7402),i=a.Z.div(r||(r=(0,o.Z)(["\n  display: flex;\n  align-items: center;\n  justify-content: center;\n"]))),l=a.Z.h1(c||(c=(0,o.Z)(["\n  font-size: 36px;\n"]))),s=t(184),u=function(){return(0,s.jsx)(i,{children:(0,s.jsx)(l,{children:"Welcome to the PhoneBook manager"})})}}}]);
-//# sourceMappingURL=304.1ad260c0.chunk.js.map
